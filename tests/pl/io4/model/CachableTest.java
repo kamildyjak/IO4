f718ab3db181160f126a7ae516a564efb043470d
jperek <jakub.perek@ii.uj.edu.pl>
2017-04-25 18:34:16 +0200

    further prototyping and a simple implementation of cachable

--- tests/pl/io4/model/CachableTest.java
+++ tests/pl/io4/model/CachableTest.java
@@ -4,11 +4,17 @@ package pl.io4.model;
  * Created by Kuba on 2017-04-22.
  */
 
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 
 // prototyp (w sensie jak to rozumiem):
@@ -40,24 +46,29 @@ class Employee {
 		}
 		return false;
 	}
+	@Override
+	public String toString() {
+		return id + ": " + firstName + " " + lastName + " " + hashSHA1;
+	}
 }
 
 class ModelMock implements Cachable {
-	private DatabaseMock<Employee> employees;
-	private DatabaseMock<Product> products;
+	private EmployeeDBMock employees;
+	private ProductsDBMock products;
 	//private TransactionRegister transactions;
 	//reszta baz
 
-	public ModelMock(DatabaseMock<Employee> e, DatabaseMock<Product> p) {
+	public ModelMock(EmployeeDBMock e, ProductsDBMock p) {
 		employees = e;
 		products = p;
 	}
 	public ModelMock(ModelMock mm) {
-		employees = new DatabaseMock<Employee>(mm.employees);
-		products = new DatabaseMock<Product>(mm.products);
+		employees = new EmployeeDBMock(mm.employees);
+		products = new ProductsDBMock(mm.products);
 	}
-	public boolean equals(ModelMock rhs) {
-		return employees.equals(rhs.employees) && products.equals(rhs.products);
+	@Override
+	public boolean equals(Object rhs) {
+		return products.equals(((ModelMock)rhs).products) && employees.equals(((ModelMock)rhs).employees);
 	}
 	@Override
 	public void cache() {
@@ -72,6 +83,10 @@ class ModelMock implements Cachable {
 		employees.load();
 		products.load();
 		//transactions.load();
+	}
+	@Override
+	public String toString() {
+		return employees.toString() + "\n" + products.toString();
 	}
 }
 
@@ -107,11 +122,217 @@ class DatabaseMock<T> implements Cachable {
 	}
 	@Override
 	public void cache() {
-
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream("db_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(records, writer);
+			writer.close();
+		} catch (IOException e) {}
 	}
 	@Override
 	public void load() {
-		records = new ArrayList<T>(); // so that it fails
+		try {
+			Reader reader = new InputStreamReader(new FileInputStream("db_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			Type type = new TypeToken<ArrayList<T>>(){}.getType();
+			ArrayList<T> records_ = gson.fromJson(reader, type);
+			System.out.println("inside_______");
+			for (T r : records)
+				System.out.println(((T)r).toString());
+			for (T r : records_)
+				System.out.println(((T)r).toString());
+			reader.close();
+		} catch (IOException e) {}
+	}
+	public String toString() {
+		return records.toString();
+	}
+}
+
+class ProductsDBMock implements Cachable {
+	private ArrayList<Product> records;
+	public void insert(Product r) {
+		records.add(r);
+	}
+	public ProductsDBMock() {
+		records = new ArrayList<Product>();
+	}
+	public ProductsDBMock(ProductsDBMock d) {
+		records = new ArrayList<Product>(d.records);
+	}
+	@Override
+	public boolean equals(Object o) { return o != null && o instanceof ProductsDBMock && records.equals(((ProductsDBMock) o).records); }
+	@Override
+	public void cache() {
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(records, writer);
+			writer.close();
+		} catch (IOException e) {}
+	}
+	@Override
+	public void load() {
+		try {
+			Reader reader = new InputStreamReader(new FileInputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			Type type = new TypeToken<ArrayList<Product>>(){}.getType();
+			records = gson.fromJson(reader, type);
+			reader.close();
+		} catch (IOException e) {}
+	}
+	public String toString() {
+		return records.toString();
+	}
+}
+
+class EmployeeDBMock implements Cachable {
+	private ArrayList<Employee> records;
+	public void insert(Employee r) {
+		records.add(r);
+	}
+	public EmployeeDBMock() {
+		records = new ArrayList<Employee>();
+	}
+	public EmployeeDBMock(EmployeeDBMock d) {
+		records = new ArrayList<Employee>(d.records);
+	}
+	@Override
+	public boolean equals(Object o) { return o != null && o instanceof EmployeeDBMock && records.equals(((EmployeeDBMock) o).records); }
+	@Override
+	public void cache() {
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(records, writer);
+			writer.close();
+		} catch (IOException e) {}
+	}
+	@Override
+	public void load() {
+		try {
+			Reader reader = new InputStreamReader(new FileInputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			Type type = new TypeToken<ArrayList<Employee>>(){}.getType();
+			records = gson.fromJson(reader, type);
+			reader.close();
+		} catch (IOException e) {}
+	}
+	public String toString() {
+		return records.toString();
+	}
+}
+
+
+class StringDBMock implements Cachable {
+	private ArrayList<RecordString> records;
+	public void insert(RecordString r) {
+		records.add(r);
+	}
+	public StringDBMock() {
+		records = new ArrayList<RecordString>();
+	}
+	public StringDBMock(StringDBMock d) {
+		records = new ArrayList<RecordString>(d.records);
+	}
+	@Override
+	public boolean equals(Object o) { return o != null && o instanceof StringDBMock && records.equals(((StringDBMock) o).records); }
+	@Override
+	public void cache() {
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(records, writer);
+			writer.close();
+		} catch (IOException e) {}
+	}
+	@Override
+	public void load() {
+		try {
+			Reader reader = new InputStreamReader(new FileInputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			Type type = new TypeToken<ArrayList<RecordString>>(){}.getType();
+			records = gson.fromJson(reader, type);
+			reader.close();
+		} catch (IOException e) {}
+	}
+	public String toString() {
+		return records.toString();
+	}
+}
+
+
+class IntDBMock implements Cachable {
+	private ArrayList<RecordInt> records;
+	public void insert(RecordInt r) {
+		records.add(r);
+	}
+	public IntDBMock() {
+		records = new ArrayList<RecordInt>();
+	}
+	public IntDBMock(IntDBMock d) {
+		records = new ArrayList<RecordInt>(d.records);
+	}
+	@Override
+	public boolean equals(Object o) { return o != null && o instanceof IntDBMock && records.equals(((IntDBMock) o).records); }
+	@Override
+	public void cache() {
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(records, writer);
+			writer.close();
+		} catch (IOException e) {}
+	}
+	@Override
+	public void load() {
+		try {
+			Reader reader = new InputStreamReader(new FileInputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			Type type = new TypeToken<ArrayList<RecordInt>>(){}.getType();
+			records = gson.fromJson(reader, type);
+			reader.close();
+		} catch (IOException e) {}
+	}
+	public String toString() {
+		return records.toString();
+	}
+}
+
+class FloatingDBMock implements Cachable {
+	private ArrayList<RecordDouble> records;
+	public void insert(RecordDouble r) {
+		records.add(r);
+	}
+	public FloatingDBMock() {
+		records = new ArrayList<RecordDouble>();
+	}
+	public FloatingDBMock(FloatingDBMock d) {
+		records = new ArrayList<RecordDouble>(d.records);
+	}
+	@Override
+	public boolean equals(Object o) { return o != null && o instanceof FloatingDBMock && records.equals(((FloatingDBMock) o).records); }
+	@Override
+	public void cache() {
+		try {
+			Writer writer = new OutputStreamWriter(new FileOutputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(records, writer);
+			writer.close();
+		} catch (IOException e) {}
+	}
+	@Override
+	public void load() {
+		try {
+			Reader reader = new InputStreamReader(new FileInputStream(getClass().getName() + "_cache.json"), "UTF-8");
+			Gson gson = new GsonBuilder().create();
+			Type type = new TypeToken<ArrayList<RecordDouble>>(){}.getType();
+			records = gson.fromJson(reader, type);
+			reader.close();
+		} catch (IOException e) {}
+	}
+	public String toString() {
+		return records.toString();
 	}
 }
 
@@ -121,11 +342,15 @@ class RecordInt {
 	RecordInt(int s1, int s2, int s3) {v1 = s1; v2 = s2; v3 = s3;}
 	@Override
 	public boolean equals(Object o) {
-		if (o != null && o instanceof RecordDouble) {
-			RecordDouble rd = (RecordDouble)o;
+		if (o != null && o instanceof RecordInt) {
+			RecordInt rd = (RecordInt)o;
 			return v1 == rd.v1 && v2 == rd.v2 && v3 == rd.v3;
 		}
 		return false;
+	}
+	@Override
+	public String toString() {
+		return v1 + " " + v2 + " " + v3;
 	}
 }
 
@@ -140,6 +365,10 @@ class RecordDouble {
 		}
 		return false;
 	}
+	@Override
+	public String toString() {
+		return v1 + " " + v2 + " " + v3;
+	}
 }
 
 class RecordString {
@@ -147,36 +376,95 @@ class RecordString {
 	RecordString(String s1, String s2, String s3) {v1 = s1; v2 = s2; v3 = s3;}
 	@Override
 	public boolean equals(Object o) {
-		if (o != null && o instanceof RecordDouble) {
-			RecordDouble rd = (RecordDouble)o;
+		if (o != null && o instanceof RecordString) {
+			RecordString rd = (RecordString)o;
 			return v1.equals(rd.v1) && v2.equals(rd.v2) && v3.equals(rd.v3);
 		}
 		return false;
+	}
+	@Override
+	public String toString() {
+		return v1 + " " + v2 + " " + v3;
 	}
 }
 
 // wlasciwe testy
 public class CachableTest {
+	
+	@Test
+	public void IdenticalIntRecordEqualityTest() {
+		RecordInt r1 = new RecordInt(1, 2, 3);
+		RecordInt r2 = new RecordInt(1, 2, 3);
+		assertEquals(r1, r2);
+	}
+
+	@Test
+	public void IdenticalDoubleRecordEqualityTest() {
+		RecordDouble r1 = new RecordDouble(1., 2., 3.);
+		RecordDouble r2 = new RecordDouble(1., 2., 3.);
+		assertEquals(r1, r2);
+	}
+
+	@Test
+	public void IdenticalStringRecordEqualityTest() {
+		RecordString r1 = new RecordString("a", "bb", "ccc");
+		RecordString r2 = new RecordString("a", "bb", "ccc");
+		assertEquals(r1, r2);
+	}
+
+	@Test
+	public void IdenticalProductEqualityTest() {
+		Product p1 = new Product("id1", "nazwa", 1.);
+		Product p2 = new Product("id1", "nazwa", 1.);
+		System.out.println(p1.toString());
+		assertEquals(p1, p2);
+	}
+
+	@Test
+	public void IdenticalEmployeeEqualityTest() {
+		Employee e1 = new Employee(1, "Asdf", "Qwerty", "asdfg");
+		Employee e2 = new Employee(1, "Asdf", "Qwerty", "asdfg");
+		assertEquals(e1, e2);
+	}
+
+	@Test
+	public void IdenticalModelMockEqualityTest() {
+		EmployeeDBMock employees1 = new EmployeeDBMock();
+		employees1.insert(new Employee(1, "Jan", "Kowalski", "0eba5756cd43e35ded410d5b49a870d574eb49d9"));
+		EmployeeDBMock employees2 = new EmployeeDBMock();
+		employees2.insert(new Employee(1, "Jan", "Kowalski", "0eba5756cd43e35ded410d5b49a870d574eb49d9"));
+
+		ProductsDBMock products1 = new ProductsDBMock();
+		products1.insert(new Product("asdf1", "czipsy Mniam", 3.45));
+		ProductsDBMock products2 = new ProductsDBMock();
+		products2.insert(new Product("asdf1", "czipsy Mniam", 3.45));
+
+		ModelMock mm1 = new ModelMock(employees1, products1);
+		ModelMock mm2 = new ModelMock(employees2, products2);
+		assertEquals(mm1, mm2);
+	}
 
 	@Test
 	public void EmptyDBTest() {
-		ModelMock mm1 = new ModelMock(new DatabaseMock<Employee>(), new DatabaseMock<Product>());
+		ModelMock mm1 = new ModelMock(new EmployeeDBMock(), new ProductsDBMock());
 		mm1.cache();
 		mm1.load();
 
-		ModelMock mm2 = new ModelMock(new DatabaseMock<Employee>(), new DatabaseMock<Product>());
+		ModelMock mm2 = new ModelMock(new EmployeeDBMock(), new ProductsDBMock());
 
-		assertEquals(true, mm1.equals(mm2));
+		//System.out.println("1:" + mm1.toString());
+		//System.out.println("2:" + mm2.toString());
+		assertEquals(mm2, mm1);
 	}
 
 	@Test
 	public void GeneralDBTest() {
-		DatabaseMock<Employee> employees = new DatabaseMock<Employee>();
+		EmployeeDBMock employees = new EmployeeDBMock();
 		employees.insert(new Employee(1, "Jan", "Kowalski", "0eba5756cd43e35ded410d5b49a870d574eb49d9"));
-		employees.insert(new Employee(1, "Bashar", "Al-Assad", "3da541559918a808c2402bba5012f6c60b27661c"));
-		employees.insert(new Employee(1, "Suhail", "Al-Hasan", "3da541559918a808c2402bba5012f6c60b27661c"));
+		employees.insert(new Employee(2, "Bashar", "Al-Assad", "3da541559918a808c2402bba5012f6c60b27661c"));
+		employees.insert(new Employee(3, "Suhail", "Al-Hasan", "3da541559918a808c2402bba5012f6c60b27661c"));
 
-		DatabaseMock<Product> products = new DatabaseMock<Product>();
+		ProductsDBMock products = new ProductsDBMock();
 		products.insert(new Product("asdf1", "czipsy Mniam", 3.45));
 		products.insert(new Product("asdf2", "ziemniak ąęćż", 1.23));
 		products.insert(new Product("asdf3", "Coca-Cola®", 0.01));
@@ -186,102 +474,120 @@ public class CachableTest {
 		mm1.cache();
 		mm1.load();
 
-		assertEquals(true, mm1.equals(mm2));
+		System.out.println("1:" + mm1.toString());
+		System.out.println("2:" + mm2.toString());
+		assertEquals(mm2, mm1);
 	}
 
 	@Test
 	public void IntsDBTest() {
-		DatabaseMock<RecordInt> db1 = new DatabaseMock<RecordInt>();
+		IntDBMock db1 = new IntDBMock();
 		db1.insert(new RecordInt(1, 2, 434243242));
 		db1.insert(new RecordInt(10, 0, 1));
-		DatabaseMock<RecordInt> db2 = new DatabaseMock<RecordInt>(db1);
+		IntDBMock db2 = new IntDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void NegativeIntsDBTest() {
-		DatabaseMock<RecordInt> db1 = new DatabaseMock<RecordInt>();
+		IntDBMock db1 = new IntDBMock();
 		db1.insert(new RecordInt(-1, -2, -434243242));
 		db1.insert(new RecordInt(-10, -0, 0));
-		DatabaseMock<RecordInt> db2 = new DatabaseMock<RecordInt>(db1);
+		IntDBMock db2 = new IntDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void FloatingPointDBTest() {
-		DatabaseMock<RecordDouble> db1 = new DatabaseMock<RecordDouble>();
+		FloatingDBMock db1 = new FloatingDBMock();
 		db1.insert(new RecordDouble(1., 2.43214123, 43424.3242));
 		db1.insert(new RecordDouble(1.70, 0., 0.0000000001));
-		DatabaseMock<RecordDouble> db2 = new DatabaseMock<RecordDouble>(db1);
+		FloatingDBMock db2 = new FloatingDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void EmptyStringDBTest() {
-		DatabaseMock<RecordString> db1 = new DatabaseMock<RecordString>();
-		DatabaseMock<RecordString> db2 = new DatabaseMock<RecordString>(db1);
+		StringDBMock db1 = new StringDBMock();
+		StringDBMock db2 = new StringDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void EmptyStringsThemselvesStringDBTest() {
-		DatabaseMock<RecordString> db1 = new DatabaseMock<RecordString>();
+		StringDBMock db1 = new StringDBMock();
 		db1.insert(new RecordString("", "", ""));
 		db1.insert(new RecordString("", "", ""));
-		DatabaseMock<RecordString> db2 = new DatabaseMock<RecordString>(db1);
+		StringDBMock db2 = new StringDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void StringDBTest() {
-		DatabaseMock<RecordString> db1 = new DatabaseMock<RecordString>();
+		StringDBMock db1 = new StringDBMock();
 		db1.insert(new RecordString("asdf", "qwerty", "abcdefgh"));
 		db1.insert(new RecordString("aaaa", "bbbb", "aaaa"));
 		db1.insert(new RecordString("1234", "2137", "1337"));
-		DatabaseMock<RecordString> db2 = new DatabaseMock<RecordString>(db1);
+		StringDBMock db2 = new StringDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void StringQuotesDBTest() {
-		DatabaseMock<RecordString> db1 = new DatabaseMock<RecordString>();
+		StringDBMock db1 = new StringDBMock();
 		db1.insert(new RecordString("", "\"", "\"\""));
 		db1.insert(new RecordString("\"\"\"", ",\":", ":\"\","));
 		db1.insert(new RecordString("", "\"", "\"\""));
-		DatabaseMock<RecordString> db2 = new DatabaseMock<RecordString>(db1);
+		StringDBMock db2 = new StringDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		System.out.println("1:" + db1.toString());
+		System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 
 	@Test
 	public void StringNewLinesDBTest() {
-		DatabaseMock<RecordString> db1 = new DatabaseMock<RecordString>();
+		StringDBMock db1 = new StringDBMock();
 		db1.insert(new RecordString("\n", "\n", "\n"));
 		db1.insert(new RecordString("\n\n\n", "", "\n\n"));
-		DatabaseMock<RecordString> db2 = new DatabaseMock<RecordString>(db1);
+		StringDBMock db2 = new StringDBMock(db1);
 		db1.cache();
 		db1.load();
 
-		assertEquals(true, db1.equals(db2));
+		//System.out.println("1:" + db1.toString());
+		//System.out.println("2:" + db2.toString());
+		assertEquals(db2, db1);
 	}
 }
