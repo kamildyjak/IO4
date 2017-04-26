4dcd19203855f2f5f612286a2f9c898308c57198
Dyju711 <kamildyj@gmail.com>
2017-04-26 03:44:57 +0200

    Poprawiony widok logowania
    Wykorzystanie biblioteki Libgdx oraz klasy LoginMachine

--- src/pl/io4/controllers/LoginController.java
+++ src/pl/io4/controllers/LoginController.java
@@ -1,63 +1,54 @@
 package pl.io4.controllers;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import pl.io4.NextGen;
+import pl.io4.model.LoginMachine;
 import pl.io4.views.LoginView;
 
 /**
  * Created by kamil on 12.04.2017.
  */
-public class LoginController extends Controller implements  Input.TextInputListener{
+public class LoginController extends Controller {
 
-    boolean login = true;
-    boolean succes = false;
-    String logintxt;
-    String passwordtxt;
-    String goodLogin = "Kamil";
-    String goodPassword = "Dyju";
-    int count = 0;
+    private String login;
+    private String password;
     private LoginView view;
+    private LoginMachine loginMachine;
+    private boolean succes;
+    private int count;
 
 
     public LoginController(NextGen app) throws NoSuchElementException{
         super(app);
         view = (LoginView) super.view;
+        loginMachine = new LoginMachine();
+        count = 0;
 
-        Gdx.input.getTextInput(this, "Login", "", "Wprowadź login");
-
-    }
-
-    @Override
-    public void input(String text){
-        if(count > 4){
-            view.loginEroor();
-            return;
-        }
-        if(login){
-            logintxt = text;
-            System.out.println("Login: "+text);
-            if(text.equals(goodLogin)) {
-                login = false;
-                Gdx.input.getTextInput(this, "Hasło", "", "Wprowadź hasło");
-            }else{
-                Gdx.input.getTextInput(this, "Błąd loginu", "", "Wprowadż ponownie login");
-                count++;
+        addButtonClickListener("submit", new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                if(count > 4){
+                    view.TooManyErrors();
+                    return;
+                }
+                login = view.GetLogin();
+                password = view.GetPassword();
+                if(login == null || password == null){
+                    view.EmptyInput();
+                    count++;
+                }else{
+                    succes = loginMachine.checkPassword(login, password);
+                    if(succes){
+                        view.Succes();
+                    }else{
+                        view.LoginError();
+                        count++;
+                    }
+                }
             }
-        }else{
-            passwordtxt = text;
-            System.out.println("Hasło: "+text);
-            if(text.equals(goodPassword)) {
-                succes = true;
-                view.loginSucceeded();
-            }else{
-                Gdx.input.getTextInput(this, "Błąd hasła", "", "Wprowadż ponownie hasło");
-            }
-        }
+        });
+
     }
 
-    @Override
-    public void canceled() {
-        view.loginCancelled();
-    }
+
 }
--- src/pl/io4/views/LoginView.java
+++ src/pl/io4/views/LoginView.java
@@ -1,7 +1,11 @@
 package pl.io4.views;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 import static pl.io4.NextGen.V_HEIGHT;
 import static pl.io4.NextGen.V_WIDTH;
@@ -10,28 +14,99 @@ import static pl.io4.NextGen.V_WIDTH;
  * Created by kamil on 12.04.2017.
  */
 public class LoginView extends View {
-    Table inner;
+    private Table inner;
+    private TextField login;
+    private TextField password;
+    private final String[] data = new String[2];
+    private TextButton submit;
 
-
-    public void loginSucceeded(){
-        inner.add("Zostałeś zalogowany do systemu!").center();
-
+    public String GetLogin(){
+        return data[0];
     }
 
-    public void loginEroor(){
-        inner.add("Przekroczyłeś ilość błędnych prób!").center();
+    public String GetPassword(){
+        return data[1];
     }
 
-    public void loginCancelled(){
-        inner.add("Anulowałeś logowanie do systemu").center();
+    public void TooManyErrors(){
+        data[0] = data[1] = null;
+        inner.clear();
+        inner.add("Przekroczyles limit prob").center();
+    }
+
+    public void EmptyInput(){
+        data[0] = data[1] = null;
+        inner.clear();
+        inner.add("Wypełnij wszystkie pola").center();
+        inner.row();
+        LoginInputs();
+    }
+
+    public void LoginError(){
+        data[0] = data[1] = null;
+        inner.clear();
+        inner.add("Wprowadziłeś błędne dane").center();
+        inner.row();
+        LoginInputs();
+    }
+
+    public void Succes(){
+        inner.clear();
+        inner.add("Poprawnie zalogowałeś się do systemu");
     }
 
     @Override
     protected void addViewElements() {
-        inner = new Table(skin).background("white");
+        inner = new Table(skin).background("gray");
         inner.defaults().pad(10);
         ScrollPane scroll = new ScrollPane(inner);
         table.add(scroll).expand().left().bottom().width((int)(V_WIDTH)).height(V_HEIGHT);
 
+        skin.add("textfieldback", new Texture("assets/raw/bg.png"));
+        skin.add("cursor", new Texture("assets/raw/cursor.png"));
+        skin.add("selection", new Texture("assets/raw/selection.png"));
+        submit = new TextButton("Zaloguj", skin);
+        LoginInputs();
+    }
+
+    public void LoginInputs(){
+        TextField.TextFieldStyle textfieldstyle = new TextField.TextFieldStyle();
+        textfieldstyle.background = skin.getDrawable("textfieldback");
+        textfieldstyle.disabledFontColor= Color.BLACK;
+        textfieldstyle.font = skin.getFont("default");
+        textfieldstyle.fontColor = Color.WHITE;
+        textfieldstyle.cursor = skin.getDrawable("cursor");
+        textfieldstyle.selection = skin.getDrawable("selection");
+
+        login = new TextField("", textfieldstyle);
+        login.setMessageText("Login");
+        login.setMaxLength(20);
+        login.setTextFieldListener(new TextField.TextFieldListener() {
+            @Override
+            public void keyTyped(TextField textField, char key) {
+                data[0] = textField.getText();
+            }
+        });
+
+        password = new TextField("", textfieldstyle);
+        password.setMessageText("Password");
+        password.setPasswordMode(true);
+        password.setPasswordCharacter('*');
+        password.setMaxLength(20);
+
+        password.setTextFieldListener(new TextField.TextFieldListener() {
+            @Override
+            public void keyTyped(TextField textField, char key) {
+                data[1] = textField.getText();
+            }
+        });
+
+        inner.add(login).width(300).height(30);
+        inner.row();
+        inner.add(password).width(300).height(30);
+        inner.row();
+        inner.add(submit).center();
+        if(!interactiveElements.containsValue("submit"))
+            interactiveElements.put("submit", submit);
     }
 }
