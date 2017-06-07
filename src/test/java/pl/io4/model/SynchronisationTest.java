f31e4ab0df64289dc40b31ff0b68fd030e4b3116
Dyju711 <kamildyj@gmail.com>
2017-06-07 03:40:44 +0200

    Test synchronizacji
    Z pewnością do poprawy

--- src/test/java/pl/io4/model/SynchronisationTest.java
+++ src/test/java/pl/io4/model/SynchronisationTest.java
@@ -1,9 +1,49 @@
 package pl.io4.model;
 
+
+import org.junit.Test;
+import pl.io4.model.database.Database;
+import pl.io4.model.database.queries.TestQuery;
+import pl.io4.model.entities.Employee;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by kamil on 05.06.2017.
  */
 public class SynchronisationTest {
 
+    static final String sha1 = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8";
+    static final String md5 = "5f4dcc3b5aa765d61d8327deb882cf99";
+    Database db = Model.getDatabase();
+
+    static final Employee testEmployee = new Employee(
+            "12345678910", "Andrzej", "Testowy",
+            "test@gmail.com", sha1, md5, "" );
+
+
+    static final Employee dbEmployee = new Employee(
+            "12345678910", "Andrzej", "Testowy",
+            "test@gmail.com", sha1, md5, "" );
+
+
+    @Test
+    public void connectionTest() throws Exception {
+        db.start();
+        db.sendQuery(new TestQuery(), new Database.Callback(){
+            public void run(){
+                assertTrue(db.isConnected());
+                db.end();
+            }
+        });
+        db.join();
+    }
+
+    @Test
+    public void changeTest(){
+        testEmployee.setFirstName("Zbyszek");
+        assertEquals(testEmployee, dbEmployee);
+    }
 
 }
